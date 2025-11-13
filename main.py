7b657bb452ec99f6e8198ea3b4b970ef751917f5
Luis <100473510+carlos8788@users.noreply.github.com>
2025-11-13 20:26:45 -0300

    nuevos archivos

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 def saludar():
     print('Hola mundo')
-    
+
+def saludar_con_parametro(nombre):
+    print(f'Hola como estas {nombre}')
+
 if __name__ == '__main__':
     saludar()
+    saludar_con_parametro('Luis')
